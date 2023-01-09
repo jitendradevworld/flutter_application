b59325c0afd44d33c5b932a8fc4a864c0f8d9f12
JITENDRA <jitendra.logistifie@gmail.com>
2023-01-09 16:36:22 +0530

    Day-2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "Ram";
+  // double pi = 3.14;
+  bool isMale = true;
+  num temp = 30.5;
+  //var day = "Monday";
+  //const pi = 3.14;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days of Flutter... by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
